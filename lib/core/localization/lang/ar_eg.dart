8ea72dbd14a81e043de2f2adeedb474451b14ba1
ahmedelmaghraby1 <ahmedelmaghraby2014@gmail.com>
2024-06-16 05:15:52 +0300

    lang

--- lib/core/localization/lang/ar_eg.dart
+++ lib/core/localization/lang/ar_eg.dart
@@ -3,7 +3,7 @@ final Map<String, String> arEg = {
   'language': 'اللغة',
   'searchWithCity': 'أدخل اسم مدينة ',
   'cantBeEmpty': 'الحقل فارغ',
-  'noCityMatches': 'لا نوحد مدينة بهذا الاسم أو باسم مشابه',
+  'noCityMatches': 'لا توجد مدينة بهذا الاسم أو باسم مشابه',
   'tryAgain': 'حاول مرة اخري',
   'noInternet': 'لا يوجد اتصال بالانترنت',
 };
